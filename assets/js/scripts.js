fdd1c0f722fe4ea1c40dece0e4d61dce2782380f
Adriano <adrianogotardo.12@gmail.com>
2022-09-24 23:22:32 -0300

    fix bugs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,17 @@
 function entering(nick) {
-    const nickRequest = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', nick);
-
+    console.log('entrou na entering');
+    const nickRequest = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', { name: nick });
+    console.log('voltou do request');
     nickRequest.then((res) => {
+        console.log(res);
         if (res.status === 400) {
+            console.log('status 400');
             alert('Name already in use, please choose a new one.')
             return;
         }
         else if (res.status === 200) {
-            () => document.getElementById("pageContent").innerHTML = `
+            console.log('status 200');
+            document.getElementById("pageContent").innerHTML = `
                 <ul id="messageList">
                 </ul>
             `;
@@ -15,26 +19,28 @@ function entering(nick) {
             return;
         }
         else {
+            console.log(res.status);
             alert('Unknown error');
             return;
         }
-        console.log(res.status)
     }).catch((error) => {
-        console.log(res.status);
+        console.log(error.message);
     });
 }
 
-function getName() {
-    const nickname = document.getElementById("nickname");
+function getName(event) {
+    event.preventDefault();
+    const nickname = document.getElementById("nickname").value;
     entering(nickname);
 }
 
 function chatRoom(nickname) {
+    console.log('entrou na chatRoom');
     setInterval(() => {
         const online = axios.post(
             "https://mock-api.driven.com.br/api/v4/uol/status",
             {
-                nickname: nickname
+                name: nickname
             });
     }, 5000, nickname);
 
@@ -42,12 +48,12 @@ function chatRoom(nickname) {
 }
 
 function getMessages() {
-    const messages = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    const messagesRequest = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
 
-    messages.then((res) => {
-        showMessages(messages);
+    messagesRequest.then((res) => {
+        showMessages(res.data);
     }).catch((error) => {
-        console.log(messages.status);
+        console.log(error.status);
     });
 }
 
@@ -64,40 +70,40 @@ function sendMessage() {
     );
 }
 
-function showMessages() {
+function showMessages(messages) {
     const pageContent = document.getElementById("messageList");
-
-    for (let i = 0; i<messages.lenght; i++) {
-        if (messages(i).type === "status") {
+    console.log(messages);
+    for (let i = 0; i < messages.length; i++) {
+        if (messages[i].type === "status") {
             pageContent.innerHTML += `
             <li class="messages status">
-                <spam class="time">${message.time}</spam>
-                <spam class="from">${message.from}</spam>
+                <spam class="time">${messages[i].time}</spam>
+                <spam class="from">${messages[i].from}</spam>
                 <spam>to</spam>
-                <spam class="to">${message.to}</spam>
-                ${message.text}
+                <spam class="to">${messages[i].to}</spam>
+                ${messages[i].text}
             </li>
             `;
         }
-        else if (messages(i).to === "private_message") {
+        else if (messages[i].type === "private_message") {
             pageContent.innerHTML += `
             <li class="messages privateMessage">
-                <spam class="time">${message.time}</spam>
-                <spam class="from">${message.from}</spam>
+                <spam class="time">${messages[i].time}</spam>
+                <spam class="from">${messages[i].from}</spam>
                 <spam>to</spam>
-                <spam class="to">${message.to}</spam>
-                ${message.text}
+                <spam class="to">${messages[i].to}</spam>
+                ${messages[i].text}
             </li>
             `;
         }
         else {
             pageContent.innerHTML += `
             <li class="messages">
-                <spam class="time">${message.time} </spam>
-                <spam class="from">${message.from} </spam>
+                <spam class="time">${messages[i].time} </spam>
+                <spam class="from">${messages[i].from} </spam>
                 <spam>to </spam>
-                <spam class="to">${message.to} </spam>
-                <spam class="message">${message.text}</spam>
+                <spam class="to">${messages[i].to} </spam>
+                <spam class="message">${messages[i].text}</spam>
             </li>
             `;
         }
